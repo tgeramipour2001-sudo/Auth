c145039e9356479dd44d004eaf84012485e2dcae
tgeramipour2001-sudo <t.geramipour2001@gmail.com>
2026-08-14 10:52:42 +0330

    Add bottom navigation

--- lib/ui/Main/mainScreen.dart
+++ lib/ui/Main/mainScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:login/ui/customers/customersList.dart';
 import 'package:login/ui/home.dart';
-import 'package:login/ui/order/orders.dart';
+import 'package:login/ui/orders/orders.dart';
 import 'package:login/ui/setting/setting.dart';
 import 'package:login/widget/bottom_navigation.dart';
 
