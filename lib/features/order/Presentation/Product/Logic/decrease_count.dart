6faadbf951caf8b41372157c78f0e3888a1984b0
Taraneh <t.geramipour2001@gmail.com>
2026-09-21 19:39:07 +0330

    Delete lib/features/order/Presentation directory

--- lib/features/order/Presentation/Product/Logic/decrease_count.dart
+++
@@ -1,9 +0,0 @@
-String DeceaseCount(String value, int minValue){
-  int count = int.tryParse(value) ?? 0;
-
-  if(count <= minValue){
-    return count.toString();
-  }else{
-    return (count - 1).toString();
-  }
-}
--- lib/features/order/Presentation/Product/Logic/increase_count.dart
+++
@@ -1,9 +0,0 @@
-String IncreaseCount(String value, int maxValue) {
-  int count = int.tryParse(value) ?? 0;
-
-  if(count >= maxValue){
-    return value;
-  }else{
-  return (count + 1).toString();
-  }
-}
