107d5abde7b4b7fb51ede859111c669576f0840d
Taraneh <t.geramipour2001@gmail.com>
2026-09-21 10:52:25 +0330

    Delete lib/features/customers/Domain directory

--- lib/features/customers/Domain/Repository/i_customer_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:login/features/customers/domain/entity/customer_entity.dart';
-
-abstract class ICustomerRepository {
-  Future<List<CustomerEntity>> getAll();
-  Future<List<CustomerEntity>> searchCustomer(String name);
-}
